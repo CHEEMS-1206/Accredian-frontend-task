48e14e96b9cdf07b842510173fdff79b8702c16e
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-12-10 18:44:31 +0530

    defined logic for custom rendering of login, register and error page?

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from "react";
+import Login from "./Components/LoginPage/Login.js";
+import Register from "./Components/RegisterPage/Register.js";
+
+import { Route, Routes, BrowserRouter } from "react-router-dom"
 
 function App() {
+  const isLoggedIn = localStorage.token ? true : false;
   return (
     <div className="App">
-      Hello world
+      <BrowserRouter>
+        <Routes>
+          {isLoggedIn && (
+            <Route
+              path="/"
+              element={<div className="homepage">Welcome to Accredian !</div>}
+            />
+          )}
+          {!isLoggedIn && (
+            <Route path="/" element={<Login/>}/>
+          )}
+          <Route path="/login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route
+            path="*"
+            element={<div className="errPage">#404 Not Found !</div>}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
